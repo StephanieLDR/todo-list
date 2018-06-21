5efd28e2488f6664fb6647ead3085fb399e0b14f
StephanieLDR <stephanie.c.lacina@gmail.com>
2018-06-20 18:03:54 -0700

    played with to-do list

--- src/components/list.js
+++ src/components/list.js
@@ -1,13 +1,13 @@
 import React from "react";
-import listData from "../data/list";
+
 
 export default (props) => {
 
 
 //listElements is making new array
 
-    const listElements = listData.map((item, index)=>{
-        return <li className="collection-item" key={item.id}>{item.title}</li>
+    const listElements = props.data.map((item, index)=>{
+        return <li className="collection-item" key={index}>{item.title}</li>
     });
 
     return (
